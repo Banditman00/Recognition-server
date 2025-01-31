1e01163156aac0dd7f589b80d77bdbed3202995b
Banditman00 <nazare.tiberiu@gmail.com>
2025-01-31 09:49:09 +0200

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : '0.0.0.0',
     user : 'postgres',
     password : 'pula',
     database : 'smart-brain',
